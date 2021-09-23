540c0b5ee5f742a767f2027598f6343fe007d07b
Joshua Austin <Joshua.Austin@students.mq.edu.au>
2021-09-23 20:56:29 +1000

    another test

--- tests/testthat/test-fbind.R
+++ tests/testthat/test-fbind.R
@@ -3,7 +3,9 @@ test_that("fbind() binds factor (or character)", {
   x_fact <- factor(x)
   y <- c("c", "d")
   z <- factor(c("a", "b", "c", "d"))
+  m <- factor(c("c", "d", "e", "f", "Q", "P", "O", "N"))
 
   expect_identical(fbind(x, y), z)
   expect_identical(fbind(x_fact, y), z)
+  expect_equal(fbind(letters[3:6], LETTERS[17:14]), m)
 })
